0d5aaa837e9a46e0fddc652fd7f5fe33a48ff4d8
Mairon <mariusz.rogawski@gmail.com>
2021-02-04 13:39:21 +0100

    PATHS IS DONE, WORKING ON CLASS CHECK FILE

--- logic.py
+++ logic.py
@@ -3,3 +3,99 @@ from tkinter import ttk
 from tkcalendar import *
 from tkinter import filedialog
 import datetime, os, stat
+
+# KLASA ODPOWIADAJĄCA ZA WCZYTYWANIE ŚCIEŻEK ORAZ FORMATÓW, ZAPIS USTAWIEŃ DO PLIKU I ICH ODCZYT Z PLIKU
+
+
+class MyPath(object):
+    pos_col = 0
+    pos_row = 0
+    num_of_paths = 1
+
+    def __init__(self, frame_paths):
+
+        self.frame_path_formats = ttk.LabelFrame(frame_paths,
+                                                 text=f"Ścieżki {MyPath.num_of_paths}A-{MyPath.num_of_paths}B")
+        self.frame_path_formats.grid(column=0, row=MyPath.pos_row)
+
+        self.my_entry1 = ttk.Entry(self.frame_path_formats, text="", width=20)
+        self.my_entry1.grid(column=1, row=0, padx=20)
+        self.my_entry2 = ttk.Entry(self.frame_path_formats, text="", width=20)
+        self.my_entry2.grid(column=2, row=0, padx=20)
+        self.my_entry3 = ttk.Entry(self.frame_path_formats, text="", width=20)
+        self.my_entry3.grid(column=2, row=3, pady=10, padx=20)
+
+        self.path_A = ""
+        self.path_B = ""
+
+        self.my_button1 = ttk.Button(self.frame_path_formats, text="Wskaż ścieżkę A",
+                                     command=lambda: self.set_path("A"))
+        self.my_button1.grid(column=1, row=1, padx=20)
+        self.my_button2 = ttk.Button(self.frame_path_formats, text="Wskaż ścieżkę B",
+                                     command=lambda: self.set_path("B"))
+        self.my_button2.grid(column=2, row=1, padx=20)
+
+        self.frame_formats = ttk.Frame(self.frame_path_formats)
+        self.frame_formats.grid(column=0, row=2, columnspan=4)
+
+        self.my_label = ttk.Label(self.frame_path_formats, text="Podaj rozszerzenia plków po przecinku, bez spacji i"
+                                                                   " kropki. Np. txt,docx,pdf,xlsx -->", wraplength=300)
+        self.my_label.grid(column=0, row=3, pady=10, padx=20, columnspan=2)
+
+        MyPath.pos_row += 3
+        MyPath.num_of_paths += 1
+
+    # POZWALA NA WYBRANIE ŚCIEŻKI FOLDERU PRZY POMOCY PRZYCISKU "WSKAŻ ŚCIEŻKĘ"
+    # USTAWIA NAZWĘ ŚCIEŻKI W POLU ENTRY
+    def set_path(self, opt):
+        path = filedialog.askdirectory()
+        if opt == "A":
+            self.my_entry1.delete(0, "end")
+            self.my_entry1.insert(0, path)
+            self.path_A = path
+
+        else:
+            self.my_entry2.delete(0,"end")
+            self.my_entry2.insert(0, path)
+            self.path_B = path
+
+    # ZAPISUJE USTAWIENIA DO PLIKU TXT
+    @staticmethod
+    def save_settings(list_with_path):
+
+        print("Zapisuje ustawienia")
+        with open("setting_file.txt", 'w') as outfile:
+            for obj in list_with_path:
+                outfile.write(obj.my_entry1.get()+"\n")
+                outfile.write(obj.my_entry2.get()+"\n")
+                outfile.write(obj.my_entry3.get()+"\n")
+
+    # WCZYTUJE USTAWIENIA
+    @staticmethod
+    def load_settings(list_with_path):
+        number = 0
+        print("Wczytuje ustawienia")
+
+        try:
+            with open("setting_file.txt", 'r') as infile:
+                lines = infile.readlines()
+            for obj in list_with_path:
+                obj.my_entry1.delete(0, "end")
+                obj.my_entry1.insert(0, lines[number].rstrip("\n"))
+                obj.my_entry2.delete(0, "end")
+                obj.my_entry2.insert(0, lines[number+1].rstrip("\n"))
+                obj.my_entry3.delete(0, "end")
+                obj.my_entry3.insert(0, lines[number+2].rstrip("\n"))
+                number += 3
+        except:
+            print("Brak w folderze z programem pliku z ustawieniami setting_file.txt")
+
+
+class CheckFile(object):
+    def __init__(self, list_of_paths, start_date, end_date):
+        self.list_of_paths = list_of_paths
+        self.start_date = start_date
+        self.end_date = end_date
+
+
+
--- main.py
+++ main.py
@@ -38,23 +38,29 @@ frame_calendars.config(height=100, width=525)
 # TWORZENIE DWÓCH KALENDARZY DO WYBORU DATY POCZATKOWEJ I KONCOWEJ
 # USTAWIANIE DATY POCZĄTKOWEJ
 text_cal_1 = Label(frame_calendars, text="DATA POCZĄTKOWA")
-text_cal_1.grid(column=0, row=0, pady=10, padx=35)
+text_cal_1.grid(column=0, row=0, pady=10, padx=20)
 cal_1 = DateEntry(frame_calendars, width=12, background='darkblue', foreground='white', borderwidth=2,
                   date_pattern='y-mm-dd')
 cal_1.grid(column=1, row=0, pady=10, padx=10)
 
 # USTAWIANIE DATY KOŃCOWEJ
 text_cal_2 = Label(frame_calendars, text="DATA KOŃCOWA")
-text_cal_2.grid(column=0, row=1, pady=10, padx=35)
+text_cal_2.grid(column=0, row=1, pady=10, padx=20)
 cal_2 = DateEntry(frame_calendars, width=12, background='darkblue', foreground='white', borderwidth=2,
                   date_pattern='y-mm-dd')
 cal_2.grid(column=1, row=1, pady=10, padx=10)
-3
+
 # RAMKA DLA PRZYCISKÓW
 frame_buttons = ttk.LabelFrame(frame1, text="Sprawdź różnice w folderach, zapisz raport z wynikami "
                                             "lub wyjdź z programu")
 frame_buttons.grid(column=0, row=1, pady=10, padx=10)
 frame_buttons.config(height=100, width=525)
+
+# RAMKA DLA ŚCIEŻEK I FORMATÓW
+frame_paths = ttk.LabelFrame(frame2, text="Wybierz ścieżki folderów")
+frame_paths.grid(column=0, row=2, columnspan=1,  pady=10)
+frame_paths.config(height=600, width=545)
+frame_paths.config(relief=RIDGE)
 
 # UTWORZENIE GŁÓWNYCH PRZYCISKÓW
 main_batton_start = ttk.Button(frame_buttons, text="PORÓWNAJ FOLDERY")
@@ -65,9 +71,9 @@ main_batton_raport = ttk.Button(frame_buttons, text="ZAPISZ RAPORT")
 main_batton_raport.grid(column=1, row=0, padx=18, pady=10)
 main_batton_exit = ttk.Button(frame_buttons, text="WYJDŹ Z PROGRAMU", command=root.quit)
 main_batton_exit.grid(column=2, row=0, padx=18, pady=10)
-# main_batton_save = ttk.Button(frame_paths, text="ZAPISZ USTAWIENIA",
-# command=lambda : dif.MyPath.save_settings(list_of_paths))
-# main_batton_save.grid(column = 0, row= 14)
+main_batton_save = ttk.Button(frame_paths, text="ZAPISZ USTAWIENIA",
+                              command=lambda: logic.MyPath.save_settings(list_of_paths))
+main_batton_save.grid(column=0, row=14)
 
 # RAMKA DLA WYŚWIETLANIA INFORMACJI O SPÓJNOŚCI FOLDEÓRW
 frame_infos = ttk.LabelFrame(frame1, text="Status spójności folderów")
@@ -80,11 +86,19 @@ frame_infos.config(height=200, width=525)
 pair_of_paths = []
 dif_status = []
 
+# LISTA PRZECHOWUJACA OBIEKTY Z PRZYCISKAMI DO WSKAZYWANIA SCIEZEK I FORMATÓW PLIKÓW
+list_of_paths = []
+
 for i in range(4):
     pair_of_paths.append(Label(frame_infos, text=f"FOLDERY {i+1}A-{i+1}B"))
     pair_of_paths[i].grid(column=0, row=i, pady=10, padx=10)
     dif_status.append(Label(frame_infos, text="-"))
     dif_status[i].grid(column=1, row=i, pady=10, padx=10)
+
+    list_of_paths.append(logic.MyPath(frame_paths))
+
+# ZAŁADOWANIE ZAPISANYCH USTAWIEŃ
+logic.MyPath.load_settings(list_of_paths)
 
 # RAMKA DLA WYŚWIETLANIA INFORMACJI O RAPORCIE
 frame_raport = ttk.LabelFrame(frame1, text="Informacja o raporcie")
@@ -92,16 +106,12 @@ frame_raport.grid(column=0, row=3,  pady=10, padx=10)
 frame_raport.config(height=120, width=525)
 
 # WYDRUK INFORMACJI O PLIKU RAPORTU I ŚCIEŻCE W KTÓREJ ZOSTAŁ UTWORZONY
+raport_status = Label(frame_raport, text="Trwa zapis raportu...")
+raport_status.grid(column=0, row=0)
 raport_info_1 = Label(frame_raport, text="Nazwa pliku: raport_file_01-02-2021_12-47-39.txt")
-raport_info_1.grid(column=0, row=0)
+raport_info_1.grid(column=0, row=1)
 raport_info_2 = Label(frame_raport, text="Ścieżka: /home/mariusz/PycharmProjects/DIFFOL/venv/bin/python "
                                          "/home/mariusz/PycharmProjects/DIFFOL/main.py", wraplength=500)
-raport_info_2.grid(column=0, row=1)
-
-
-
-
-
-
+raport_info_2.grid(column=0, row=2)
 
 root.mainloop()
